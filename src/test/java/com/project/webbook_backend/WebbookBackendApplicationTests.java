f5e24b2096532403f28ffae55ec59e90a6b49392
hungdt1509 <hungdangak1509@gmail.com>
2024-04-18 13:36:23 +0700

    first push

---
+++ src/test/java/com/project/webbook_backend/WebbookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.webbook_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbookBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
