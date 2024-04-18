eff20759db58be6665336a1855a0506543be1cba
hungdt1509 <hungdangak1509@gmail.com>
2024-04-18 13:39:59 +0700

    Second push

---
+++ src/main/java/com/project/webbook_backend/WebbookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.webbook_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbookBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebbookBackendApplication.class, args);
+	}
+
+}
